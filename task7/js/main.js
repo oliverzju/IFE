777da585d39c3aeeb3045253c76d7fe2168da06a
wsy_win <wsyzju@foxmail.com>
2017-01-18 14:21:23 +0800

    add some js in task7
    Signed-off-by: wsy_win <wsyzju@foxmail.com>

--- task7/js/main.js
+++ task7/js/main.js
@@ -1,7 +1,7 @@
 function posterOnHover(obj) {
 	obj.getElementsByTagName("img")[0].style.top = "52px";
 	obj.getElementsByTagName("p")[0].style.top = "-352px";
-	obj.getElementsByTagName("p")[0].style.left = "15px";
+	obj.getElementsByTagName("p")[0].style.paddingLeft = "30px";
 	obj.getElementsByTagName("p")[0].style.fontWeight = "bold";
 	obj.getElementsByTagName("p")[0].style.letterSpacing = "41px";
 	obj.getElementsByTagName("p")[1].style.top = "-352px";
@@ -11,7 +11,7 @@ function posterReset(obj) {
 	obj.getElementsByTagName("img")[0].style.top = "0";
 	obj.getElementsByTagName("p")[0].style.top = "0";
 	obj.getElementsByTagName("p")[1].style.top = "0";
-	obj.getElementsByTagName("p")[0].style.left = "0";
+	obj.getElementsByTagName("p")[0].style.paddingLeft = "0";
 	obj.getElementsByTagName("p")[0].style.fontWeight = "normal";
 	obj.getElementsByTagName("p")[0].style.letterSpacing = "0";
 	obj.getElementsByTagName("p")[1].style.top = "0";
